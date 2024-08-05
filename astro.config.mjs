39f4037f4593972e3aa6d88fb10d9dc22fa0e7ce
Gerartam <gerartamayocenteno@gmail.com>
2024-08-04 22:04:56 -0300

    Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel()
 });
